fae8db1a2bf2dbdf8b54ff1bcb74cfc204ea021f
Max Radermacher <max.radermacher@gmail.com>
2014-11-11 22:41:27 +0100

    Update IOTask to conform to the GSG.

--- src/simizer/processor/tasks/IOTask.java
+++ src/simizer/processor/tasks/IOTask.java
@@ -1,36 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.processor.tasks;
 
 import simizer.VM;
 
 /**
- * Task for IO operations, 
- * Describes how to configure the next Data ready Event.
- * 
+ * Task for IO operations, Describes how to configure the next Data ready Event.
+ *
  * @author Sylvain Lefebvre
  */
 public abstract class IOTask extends Task {
-    
-    
-    
-    private final int sz;
-    
-    public IOTask(int sz) {
-        super(null);
-        this.sz =  sz;
-    }
-    
 
-    public int getSize() {
-        return sz;
-    }
+  private final int sz;
 
-    @Override
-    public abstract void startTask(VM vm, long timestamp);
-    
-   
-    
+  public IOTask(int sz) {
+    super(null);
+
+    this.sz = sz;
+  }
+
+  public int getSize() {
+    return sz;
+  }
+
+  @Override
+  public abstract void startTask(VM vm, long timestamp);
 }

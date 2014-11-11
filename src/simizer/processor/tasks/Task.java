d492ccfb094d83b71b4bca0d96914c82da211447
Max Radermacher <max.radermacher@gmail.com>
2014-11-11 12:43:55 +0100

    Update the Task class to conform to the GSG.

--- src/simizer/processor/tasks/Task.java
+++ src/simizer/processor/tasks/Task.java
@@ -1,40 +1,39 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.processor.tasks;
 
 import simizer.VM;
 
 /**
- * This class is used for simulating a Task on a given node.
- * It takes the quantity of instructions to process.
- * Pass it to the processor to begin execution.
+ * This class is used for simulating a Task on a given node. It takes the
+ * quantity of instructions to process. Pass it to the processor to begin
+ * execution.
+ *
  * @author Sylvain Lefebvre
  */
 public abstract class Task {
-    private TaskStatus status = TaskStatus.RUNNING;
-    
-    private TaskSession ts;
-    
-    public Task(TaskSession ts) {
-        this.ts = ts;
-    }
-    
-    public TaskSession getTaskSession() {
-        return ts;
-    }
-    public void setTaskSession(TaskSession ts) {
-        this.ts = ts;
-    }
-    public TaskStatus getStatus() {
-        return this.status;
-    }
-    public abstract void startTask(VM vm, long timestamp);
-    
-    public void finishTask() {
-        this.status = TaskStatus.FINISHED;
-    }
 
-    
+  private TaskStatus status = TaskStatus.RUNNING;
+
+  private TaskSession ts;
+
+  public Task(TaskSession ts) {
+    this.ts = ts;
+  }
+
+  public TaskSession getTaskSession() {
+    return ts;
+  }
+
+  public void setTaskSession(TaskSession ts) {
+    this.ts = ts;
+  }
+
+  public abstract void startTask(VM vm, long timestamp);
+
+  public void finishTask() {
+    this.status = TaskStatus.FINISHED;
+  }
+
+  public TaskStatus getStatus() {
+    return this.status;
+  }
 }

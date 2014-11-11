45905f615481ecbe2b8fbb764b081e92feb13152
Max Radermacher <max.radermacher@gmail.com>
2014-11-11 22:42:15 +0100

    Update ProcTask to conform to the GSG.

--- src/simizer/processor/tasks/ProcTask.java
+++ src/simizer/processor/tasks/ProcTask.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.processor.tasks;
 
 import simizer.VM;
@@ -9,37 +5,43 @@ import simizer.processor.ProcessingUnit;
 
 /**
  * Processing task
+ *
  * @author slefebvr
  */
 public class ProcTask extends Task {
-    private final long nbInstructions;
-    private long insLeft;
-    
-    public ProcTask(long nbInstructions, int memSize) {
-        super(null);
-        this.nbInstructions = nbInstructions;
-        this.insLeft = nbInstructions;
-    }
-    
-    public long getProcDone() {
-        return nbInstructions - insLeft;
-    }
-    public long getProcLeft() {
-        return insLeft;
-    }
-    public long getProc() {
-        return nbInstructions;
-    }
-    public void updateProc(long inst) {
-        insLeft-=inst;
-        if(insLeft<=0)
-            this.finishTask();
-    }
 
-    @Override
-    public void startTask(VM vm, long timestamp) {
-        ProcessingUnit pu = vm.getProcessingUnit();
-        pu.startProcTask(timestamp, this);
+  private final long nbInstructions;
+  private long insLeft;
+
+  public ProcTask(long nbInstructions, int memSize) {
+    super(null);
+
+    this.nbInstructions = nbInstructions;
+    this.insLeft = nbInstructions;
+  }
+
+  public long getProcDone() {
+    return nbInstructions - insLeft;
+  }
+
+  public long getProcLeft() {
+    return insLeft;
+  }
+
+  public long getProc() {
+    return nbInstructions;
+  }
+
+  public void updateProc(long inst) {
+    insLeft -= inst;
+    if (insLeft <= 0) {
+      this.finishTask();
     }
-    
+  }
+
+  @Override
+  public void startTask(VM vm, long timestamp) {
+    ProcessingUnit pu = vm.getProcessingUnit();
+    pu.startProcTask(timestamp, this);
+  }
 }

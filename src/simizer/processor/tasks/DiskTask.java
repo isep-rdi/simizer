acb4d1192f6eed3ecd949e263f33cc16e287a5ab
Max Radermacher <max.radermacher@gmail.com>
2014-11-11 22:40:29 +0100

    Update DiskTask to conform to the GSG.

--- src/simizer/processor/tasks/DiskTask.java
+++ src/simizer/processor/tasks/DiskTask.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.processor.tasks;
 
 import simizer.VM;
@@ -10,38 +6,41 @@ import simizer.storage.IOType;
 import simizer.storage.Resource;
 
 /**
- *  Task for reading or writing data to a disk.
+ * Task for reading or writing data to a disk.
+ *
  * @author isep
  */
 public class DiskTask extends IOTask {
-    private final IOType type;
-    private final Resource r;
-    
-    public DiskTask(int sz, Resource res, IOType type) {
-        super(sz);
-        
-       this.r = res;
-       this.type = type;
-      
-    }
-    
-    public IOType getType() {
-        return type;
-    }
-    public Resource getResource() {
-        return r;
-    }
-    /**
-     * Registers an IOTaskEvent through the given virtual machine event
-     * producer.
-     * 
-     * @param vm
-     * @param timestamp 
-     */
-    @Override
-    public void startTask(VM vm, long timestamp) {
-       long timing = vm.getTaskLength(this);
-      
-        vm.registerEvent(new IOTaskEvent(timestamp+timing,this, vm.getProcessingUnit()));
-    }
+
+  private final IOType type;
+  private final Resource r;
+
+  public DiskTask(int sz, Resource res, IOType type) {
+    super(sz);
+
+    this.r = res;
+    this.type = type;
+  }
+
+  public IOType getType() {
+    return type;
+  }
+
+  public Resource getResource() {
+    return r;
+  }
+
+  /**
+   * Registers an IOTaskEvent through the given virtual machine event producer.
+   *
+   * @param vm
+   * @param timestamp
+   */
+  @Override
+  public void startTask(VM vm, long timestamp) {
+    long timing = vm.getTaskLength(this);
+
+    vm.registerEvent(
+        new IOTaskEvent(timestamp + timing, this, vm.getProcessingUnit()));
+  }
 }

8ef115f7bb038b5b5ccb7a2471737048421e11a2
Max Radermacher <max.radermacher@gmail.com>
2014-11-13 18:36:11 +0100

    Update the EventProducer files to use the GSG.
    This includes both the EventProducer class and IEventProducer interface.

--- src/simizer/event/IEventProducer.java
+++ src/simizer/event/IEventProducer.java
@@ -1,15 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.event;
 
-/**
- *
- * @author isep
- */
 public interface IEventProducer {
-    public Channel getOutputChannel();
-    public void setChannel(final Channel c);
-    public void registerEvent(Event evt);
+
+  public Channel getOutputChannel();
+
+  public void setChannel(final Channel c);
+
+  public void registerEvent(Event evt);
+
 }

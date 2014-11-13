0953b1b782a84cb1a22fa52affbbf384ccd04f8c
Max Radermacher <max.radermacher@gmail.com>
2014-11-13 15:44:29 +0100

    Update EventDispatcher2 to conform to the GSG.

--- src/simizer/event/EventDispatcher2.java
+++ src/simizer/event/EventDispatcher2.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * http://www.ibm.com/developerworks/library/j-jtp0730/index.html
- */
 package simizer.event;
 
 import java.util.concurrent.TimeUnit;
@@ -10,66 +5,70 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author Sylvain
- * Generic event dispatcher, waiting on the Channel
+ * http://www.ibm.com/developerworks/library/j-jtp0730/index.html
+ * @author Sylvain Generic event dispatcher, waiting on the Channel
  */
 public class EventDispatcher2 implements Runnable {
-    private static int dispatcherCounter = 0;
-    private static long WAIT_TIME = 100;
-    private final Channel chan;
-    private final int number;
-    private boolean ended = false;
-    private int  evtCounter = 0;
-    private long clock = 0;
-    public EventDispatcher2(final Channel chan) {
-        this.number = dispatcherCounter++;
-        this.chan = chan;
-        
-    }
 
-    @Override
-    public void run() {
-        long start = System.currentTimeMillis();
-        while(!ended) {
-        
-            Event e = null;
+  private static int dispatcherCounter = 0;
+  private static long WAIT_TIME = 100;
+  private final Channel chan;
+  private final int number;
+  private boolean ended = false;
+  private int evtCounter = 0;
+  private long clock = 0;
 
-            try {
-                e = chan.poll(WAIT_TIME, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(EventDispatcher2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-               
-            if(e!=null) { // in case we wakeup and chan is empty nonetheless
-               // System.out.println(e.timestamp);
-                if(e.timestamp < clock)
-                    System.out.println("Causality error clock: " + clock + "ts : " + e.timestamp +  " Dispatcher number: " +  this.number);
-                clock = e.timestamp;
-                e.dispatch();
-                evtCounter++;
-            }
-            
-            if(chan.size()==0) {
-                stop();
-            }
-            
+  public EventDispatcher2(final Channel chan) {
+    this.number = dispatcherCounter++;
+    this.chan = chan;
+  }
+
+  @Override
+  public void run() {
+    long start = System.currentTimeMillis();
+
+    while (!ended) {
+
+      Event e = null;
+
+      try {
+        e = chan.poll(WAIT_TIME, TimeUnit.MILLISECONDS);
+      } catch (InterruptedException ex) {
+        Logger.getLogger(EventDispatcher2.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+      if (e != null) { // in case we wakeup and chan is empty nonetheless
+        // System.out.println(e.timestamp);
+        if (e.timestamp < clock) {
+          System.out.println("Causality error clock: " + clock + "ts : " + e.timestamp + " Dispatcher number: " + this.number);
         }
-        
-        System.out.println("Nothing to do here anymore..."+ evtCounter +" events in " + (System.currentTimeMillis()-start)+ "ms");
+        clock = e.timestamp;
+        e.dispatch();
+        evtCounter++;
+      }
+
+      if (chan.size() == 0) {
+        stop();
+      }
     }
-    public synchronized void stop() {
-         this.ended = true;
-        
-    }
-    public Channel getChannel() {
-        return this.chan;
-    }
-    public int getEvtCounter() {
-        return evtCounter;
-    }
-    public long getClock() {
-       return clock;
-    }
-    
+
+    System.out.println("Nothing to do here anymore..." + evtCounter + " events in " + (System.currentTimeMillis() - start) + "ms");
+  }
+
+  public synchronized void stop() {
+    this.ended = true;
+  }
+
+  public Channel getChannel() {
+    return this.chan;
+  }
+
+  public int getEvtCounter() {
+    return evtCounter;
+  }
+
+  public long getClock() {
+    return clock;
+  }
+
 }

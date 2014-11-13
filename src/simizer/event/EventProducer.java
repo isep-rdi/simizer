58fe187f8869802d13d2b93584b4d4831b6bdef8
Max Radermacher <max.radermacher@gmail.com>
2014-11-13 18:50:42 +0100

    Add documentation to EventProducer.

--- src/simizer/event/EventProducer.java
+++ src/simizer/event/EventProducer.java
@@ -1,26 +1,34 @@
 package simizer.event;
 
+/**
+ * Provides a simple class implementation of {@code IEventProducer}.
+ * <p>
+ * This class provides a class implementation of the {@link IEventProducer}
+ * interface.  This allows other classes to easily add the functionality of
+ * {@link IEventProducer} by deriving from this class.
+ */
 public class EventProducer implements IEventProducer {
 
-  protected Channel chan;
+  /** The channel used by this {@code EventProducer}. */
+  protected Channel channel;
 
   @Override
   public Channel getOutputChannel() {
-    return this.chan;
+    return this.channel;
   }
 
   @Override
-  public void registerEvent(Event evt) {
-    chan.registerEvent(evt);
+  public void registerEvent(Event event) {
+    channel.registerEvent(event);
   }
 
   @Override
-  public void setChannel(Channel c) {
-    this.chan = c;
+  public void setChannel(Channel channel) {
+    this.channel = channel;
   }
 
-  public boolean cancelEvent(Event e) {
-    return chan.remove(e);
+  public boolean cancelEvent(Event event) {
+    return channel.remove(event);
   }
 
 }

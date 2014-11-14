a893d24a9b25f4255468ad09a4a81daa145c007a
Max Radermacher <max.radermacher@gmail.com>
2014-11-14 14:36:35 +0100

    Add documentation to the Network class.

--- src/simizer/network/Network.java
+++ src/simizer/network/Network.java
@@ -11,51 +11,105 @@ import simizer.laws.Law;
 import simizer.requests.Request;
 
 /**
- * Network simulation class. A set of nodes is associated to this network. The
- * class can be set up to use the arrival law to simulate node arrivals.
+ * Simulates a Network.
+ *
+ * There is a set of nodes associated with this {@code Network}.  The class is
+ * used to facilitate sending messages / requests between nodes.  It will apply
+ * realistic delays to messages sent through the {@code Network}.
  *
  * @author Sylvain Lefebvre
  */
 public class Network extends EventProducer {
 
-  protected Law delayLaw;
-  protected Map<Integer, Node> nodeMap;
+  /** The delay that is added when sending a message across the network. */
+  protected final Law delayLaw;
 
+  /** The nodes contained in this network. */
+  protected final Map<Integer, Node> nodeMap;
+
+  /**
+   * Initializes a network with the specified nodes and delay law.
+   *
+   * @param nodeMap a {@link Map} containing {@code nodeID} -> {@link Node}
+   *            pairs that should be added
+   * @param delayLaw a {@link Law} that defines the delays added when sending
+   *            traffic through this {@code Network}
+   */
   public Network(final Map<Integer, Node> nodeMap, Law delayLaw) {
     this.delayLaw = delayLaw;
     this.nodeMap = nodeMap;
   }
 
+  /**
+   * Initializes a network without adding any initial nodes.
+   *
+   * @param delayLaw a {@link Law} that defines the delays added when sending
+   *            traffic through this {@code Network}
+   */
   public Network(Law delayLaw) {
-    this.delayLaw = delayLaw;
-    this.nodeMap = new TreeMap<>();
+    this(new TreeMap<Integer, Node>(), delayLaw);
   }
 
-  protected long getDelay() {
+  /**
+   * Generates the random delay for the next sending.
+   * <p>
+   * This makes use of the specified {@link Law} to select a random delay when
+   * sending the message.  This is to simulate network latency.
+   *
+   * @return the next delay (in milliseconds)
+   */
+  protected long generateNextDelay() {
     return delayLaw.nextValue();
   }
 
-  public void send(Node source, Node dest, Request r, long timestamp) {
-    //1. create a message and set delay
-    long delay = (long) this.getDelay();
-    // System.out.println("Delay == " + delay);
-    r.setDelay(r.getDelay() + delay);
-    //2. send through sender
-    Message m = new Message(source, dest, r);
-    registerEvent(new RequestReceivedEvent(timestamp + delay, m, dest));
-    //System.out.println("Sending from " + source + " to " + dest);
+  /**
+   * Sends the specified {@code Request} to the destination.
+   *
+   * @param source the {@code Node} where the request originated
+   * @param destination the {@code Node} where the request should be delivered
+   * @param request the {@code Request} to deliver
+   * @param timestamp the timestamp when the {@code Message} is sent
+   */
+  public void send(Node source, Node destination, Request request,
+        long timestamp) {
+
+    // add the delay to the message
+    long delay = this.generateNextDelay();
+    request.setDelay(request.getDelay() + delay);
+
+    // send it to the destination
+    Message m = new Message(source, destination, request);
+    registerEvent(new RequestReceivedEvent(timestamp + delay, m, destination));
   }
 
+  /**
+   * Look up a {@code Node} by its nodeID value.
+   * <p>
+   * This will only find nodes that are a part of this {@code Network}.
+   * 
+   * @param nodeId the ID of the {@code Node} to find
+   * @return the {@code Node} with the specified ID
+   */
   public Node getNode(Integer nodeId) {
     return nodeMap.get(nodeId);
   }
 
+  /**
+   * Returns a list of all the {@code Node} objects in this {@code Network}.
+   * 
+   * @return a list of all the {@code Node} objects in this {@code Network}
+   */
   public List<Node> getNodeList() {
     return new LinkedList(this.nodeMap.values());
   }
 
-  public void putNode(Node n) {
-    nodeMap.put(n.getId(), n);
+  /**
+   * Adds a {@code Node} to this {@code Network}.
+   *
+   * @param node the {@code Node} to add
+   */
+  public void putNode(Node node) {
+    nodeMap.put(node.getId(), node);
   }
 
 }

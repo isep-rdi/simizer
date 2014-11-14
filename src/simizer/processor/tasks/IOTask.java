887752339a83bf8397e6e959bed03606f0558e11
Max Radermacher <max.radermacher@gmail.com>
2014-11-14 11:50:22 +0100

    Add documentation to IOTask.

--- src/simizer/processor/tasks/IOTask.java
+++ src/simizer/processor/tasks/IOTask.java
@@ -3,24 +3,39 @@ package simizer.processor.tasks;
 import simizer.VM;
 
 /**
- * Task for IO operations, Describes how to configure the next Data ready Event.
+ * Represents a task that performs an IO operation.
+ * <p>
+ * This abstract class encompasses the shared behavior between all IO-related
+ * tasks, such as the requirement to have a size.
  *
  * @author Sylvain Lefebvre
  */
 public abstract class IOTask extends Task {
 
-  private final int sz;
+  /** The size of the operation. */
+  private final int size;
 
-  public IOTask(int sz) {
+  /**
+   * Initializes a new task with the specified size.
+   *
+   * @param size the size of the operation
+   */
+  public IOTask(int size) {
     super();
 
-    this.sz = sz;
+    this.size = size;
   }
 
+  /**
+   * Returns the size of the IO operation.
+   *
+   * @return the size of the IO operation
+   */
   public int getSize() {
-    return sz;
+    return size;
   }
 
   @Override
   public abstract void startTask(VM vm, long timestamp);
+
 }

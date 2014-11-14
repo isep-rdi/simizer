5e065a0117a57ee6fae36f8b730c88eb5ce9d0e9
Max Radermacher <max.radermacher@gmail.com>
2014-11-14 15:33:45 +0100

    Add documentation to the IOType enum.

--- src/simizer/storage/IOType.java
+++ src/simizer/storage/IOType.java
@@ -1,5 +1,11 @@
 package simizer.storage;
 
+/**
+ * The possible types for storage-based IO operations.
+ * <p>
+ * These are used within the {@link DiskTask} tasks to specify the kind of
+ * action that should occur.
+ */
 public enum IOType {
   READ,
   WRITE,

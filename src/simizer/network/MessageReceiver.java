25464241627cd685418cf130145e7dc5fad77e24
Max Radermacher <max.radermacher@gmail.com>
2014-11-18 22:42:58 +0100

    Update MessageReceiver to conform to the GSG.

--- src/simizer/network/MessageReceiver.java
+++ src/simizer/network/MessageReceiver.java
@@ -1,17 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.network;
 
-import simizer.network.Message;
-
 /**
- * Interface for request receiving entities.
- * Can be part of @see Network and @see Message
- * @author isep
+ * Interface for request receiving entities. Can be part of @see Network and
+ * @see Message
  */
 public interface MessageReceiver {
-    public void onMessageReceived(long timestamp,Message m);
-    
+
+  public void onMessageReceived(long timestamp, Message m);
+
 }

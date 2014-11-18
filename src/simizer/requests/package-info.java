c0ff4f48ffafc5156dc9acbca45032addfaa421a
Max Radermacher <max.radermacher@gmail.com>
2014-11-18 23:04:39 +0100

    Update the documentation for the requests package.

--- src/simizer/requests/package-info.java
+++ src/simizer/requests/package-info.java
@@ -1,21 +1,15 @@
 /**
- * Provides the classes necessary to create requests
- * and responses between nodes .
+ * Provides the necessary classes to simulate sending requests.
  * <p>
- * There are several ways of creating requests:
- * <ul>
- * <li> The default request does not imply an answer from the destination node.
- *      It is therefore asynchronous by default. (@see simizer.app.Application.sendOneWay)
- * </li>
+ * There are two main ways to create and send requests:
+ * <ul><li>The default request does not require an answer from the destination
+ *         node.  It can therefore be considered asynchronous.  See {@link
+ *         Application#sendOneWay(Node,Request)}.
  * 
- * <li> the second method demands a response from the destination node. 
- * the responding node must therefore create a Response object 
- * with the originating request as parent parameter, and call the sendResponse method
- * Upon reception a ResponseReceived event will be signaled to the VM and propagated to the Application,
- * which will the call the handle method of the corresponding Application.
+ *     <li>The second way requires a response from the destination node.  The
+ *         responding node must therefore send the request back to the caller.
+ *         (See {@link Application#sendResponse(Node,Request)}.</ul>
  * 
  * @since 0.4
- * @see simizer.VM
- * @see  simizer.app
  */
 package simizer.requests;

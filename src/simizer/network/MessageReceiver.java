5e3d728c6b815ae414302c28c5bd285bbe77213e
Max Radermacher <max.radermacher@gmail.com>
2014-11-18 22:49:23 +0100

    Add documentation to MessageReceiver.

--- src/simizer/network/MessageReceiver.java
+++ src/simizer/network/MessageReceiver.java
@@ -1,11 +1,24 @@
 package simizer.network;
 
 /**
- * Interface for request receiving entities. Can be part of @see Network and
- * @see Message
+ * Applied to objects that can be the destination of {@code Message}s.
+ * <p>
+ * This interface should be applied to items in the {@link Simulation} that are
+ * capable of receiving {@link Message} objects.  In the context of real
+ * networks, these devices would be the devices with an IP address or a MAC
+ * address.
  */
 public interface MessageReceiver {
 
-  public void onMessageReceived(long timestamp, Message m);
+  /**
+   * Processes {@code Message} objects that are received.
+   * <p>
+   * Implement this method to provide handling for {@link Message}s sent to the
+   * object.  The timestamp of delivery and {@link Message} object are provided.
+   *
+   * @param timestamp the timestamp when the {@link Message} is delivered
+   * @param message the {@link Message} being delivered
+   */
+  public void onMessageReceived(long timestamp, Message message);
 
 }

3cff00c1921e708c442446671eae993dcdbb8b46
Max Radermacher <max.radermacher@gmail.com>
2014-11-18 14:46:36 +0100

    Update Request and Response to conform to the GSG.
    This also deprecates the setId method on the classes.  Given that nature
    of IDs, these should not be changeable once set.  The related behavior
    will be updated and changed accordingly in future commits.

--- src/simizer/requests/Response.java
+++ src/simizer/requests/Response.java
@@ -1,34 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package simizer.requests;
 
 /**
- * A response is Request delagating to its parent request 
- * (the one it is responding to)
+ * A response is Request delagating to its parent request (the one it is
+ * responding to)
+ *
  * @author Sylvain Lefebvre
  */
 public class Response extends Request {
-    private final Request parent;
-    /**
-     * Default constructor, 
-     * creates a request with the same AppId as the parent request
-     * @param type
-     * @param parent 
-     */
-    public Response(String type, Request parent) {
-        this(type,parent,parent.typeId);
-    }
-    public Response(String type, Request parent, int appId) {
-        super(type);
-        this.type = type;
-        this.parent = parent;
-        this.typeId = appId;
 
-    }
-    
-    
+  /**
+   * Default constructor, creates a request with the same AppId as the parent
+   * request
+   *
+   * @param type
+   * @param parent
+   */
+  public Response(String type, Request parent) {
+    this(type, parent, parent.typeId);
+  }
+
+  public Response(String type, Request parent, int appId) {
+    super(type);
+    this.type = type;
+    this.typeId = appId;
+  }
+
 }

f76f5cde833bfc4b3af8f11bd8a6e8fdcb6facb5
Max Radermacher <max.radermacher@gmail.com>
2014-11-25 13:59:58 +0100

    Remove run and runClientSim modes from Simizer.
    These two modes rely on the presence of Policy objects and some sort of
    a load balancer.  This functionality has been removed from the core of
    the Simizer framework.  Therefore, they can't possibly function as
    intended within the confines of the framework.
    
    In addition, I believe their behavior is reliant upon the old
    architecture of the system.  I think that the behavior should either
    
      (a) be moved into the examples framework where the policies and load
          balancing application are located
    
      (b) be updated to be able to more generically run a simulation against
          some particular application.  This would allow them to be used for
          the load balancing case and also any other custom client cases.

--- src/simizer/Simizer.java
+++ src/simizer/Simizer.java
@@ -3,25 +3,16 @@ package simizer;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import simizer.event.Channel;
-import simizer.event.EventDispatcher;
-import simizer.laws.GaussianLaw;
 import simizer.laws.Law;
-import simizer.network.ClientGenerator;
-import simizer.network.Network;
-import simizer.policy.Policy;
 import simizer.requests.Request;
 import simizer.requests.RequestFactory;
-import simizer.storage.ResourceFactory;
-import simizer.storage.StorageElement;
-import simizer.utils.SimizerUtils;
 
 /**
  *
@@ -74,7 +65,7 @@ public class Simizer {
         break;
 
       case "run":
-        if (args.length < 5) {
+        if (args.length < 5 || true) {
           printUsage("run");
         } else {
           run(args);
@@ -82,7 +73,7 @@ public class Simizer {
         break;
 
       case "clientSim":
-        if (args.length < 7) {
+        if (args.length < 7 || true) {
           printUsage("clientSim");
         } else {
           clientSim(args);
@@ -90,148 +81,9 @@ public class Simizer {
     }
   }
 
-  public static Queue<Request> getRequestQueue(String requestfile, String requestDescFile) {
-
-    String[] reqTab = SimizerUtils.readRequestsFile(requestfile);
-    RequestFactory rf = null;
-    try {
-      rf = new RequestFactory(RequestFactory.loadRequests(requestDescFile));
-    } catch (IOException ex) {
-      Logger.getLogger(Simizer.class.getName()).log(Level.SEVERE, null, ex);
-      System.exit(0);
-    }
-
-    Queue<Request> rqueue = new ArrayBlockingQueue<Request>(reqTab.length);
-    for (int i = 0; i < reqTab.length; i++) {
-      //System.out.println(reqTab[i]);
-      String[] descTab = reqTab[i].split(";");
-      rqueue.add(rf.getRequest(Long.parseLong(descTab[1]),
-              Integer.parseInt(descTab[2])));
-    }
-
-    return rqueue;
-
-  }
   // Missing resource description
 
-  private static void run(String[] args) {
-    String nodeFile = args[0]; // fichier de description des noeuds
-    String reqDesc = args[1]; // description des requêtes;
-    String workload = args[2]; // description de la workload
-    String resFile = args[3]; // desc des ressources
-    String policyName = args[4]; // lb policy classname
-    // run ./5nodes.json ./reqDescription_v1.csv ./workload_gene_v1.csv ./test Simizer.policies.CawaDyn
-    run(nodeFile, reqDesc, workload, resFile, policyName);
-  }
-
-  public static void run(String nodeFile, String reqDesc, String wlFile, String resFile, String policyName) {
-
-    //1. charger la description des requêtes
-    Queue<Request> rqueue = getRequestQueue(wlFile, reqDesc);
-
-        //2. charger les storage element
-    //int NB_RES = 150;
-    ResourceFactory resfac = SimizerUtils.getRessourceFactory(resFile);
-
-        //@TODO create a storage element for each node with different ressources (RR fashion)
-    StorageElement.setFactory(resfac);
-    final StorageElement se = new StorageElement(resfac.getMax() * resfac.getResourceSize(), 6L);
-    se.write(resfac.getStartList());
-
-    resfac.getResourceSize();
-    //3. charger les noeuds
-    final Map<Integer, Node> nodeMap = new TreeMap<Integer, Node>();
-        //Network setup
-    // on cree 2 reseaux:
-    //Internet
-    Network internet = new Network(nodeMap, new GaussianLaw(75));
-    //LAN
-    Network lan = new Network(nodeMap, new GaussianLaw(5));
-
-    // policy & lb setup :
-    Policy pol = null;
-    try {
-      pol = (Policy) Class.forName(policyName).newInstance();
-      System.out.println(pol.toString());
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      Logger.getLogger(SimizerUtils.class.getName()).log(Level.SEVERE, null, ex);
-      System.exit(0);
-    }
-
-    LBNode lbn = new LBNode(-1, pol);
-
-    List<ServerNode> nodeList = SimizerUtils.decodeNodes(SimizerUtils.readFile(nodeFile));
-
-    nodeList.size();
-    final Channel c = new Channel();
-    for (ServerNode n : nodeList) {
-
-      n.setFrontendNode(lbn);
-      n.setStorage(se);
-      n.setNetwork(lan);
-      n.setChannel(c);
-      nodeMap.put(n.getId(), n);
-
-    }
-
-    pol.initialize(nodeList, lbn);
-
-    nodeMap.put(-1, lbn);
-
-    internet.setChannel(c);
-    lan.setChannel(c);
-
-    lbn.setInsideNetwork(lan);
-    lbn.setOutsideNetwork(internet);
-
-    System.out.println("Thread starting for " + rqueue.size());
-        //4.Envoi des requetes
-
-        // total number of events to be processed by all threads.
-    // this is necessary to know when to stop the threads.
-    int totalEvt = rqueue.size() * 4;
-    while (!rqueue.isEmpty()) {
-      Request r = rqueue.poll();
-      internet.send(null, lbn, r, r.getArTime());//Node source, Node dest, Request r, long timestamp
-    }
-
-    int nbCores = 1;// Runtime.getRuntime().availableProcessors() +1;
-
-    ExecutorService es = Executors.newFixedThreadPool(nbCores);
-    EventDispatcher[] edArray = new EventDispatcher[nbCores];
-
-    for (int i = 0; i < nbCores; i++) {
-      edArray[i] = new EventDispatcher(c);
-      es.submit(edArray[i]);
-    }
-
-    // waiting loop for ending the processing.
-    int sum = 0;
-    while (edArray[0].getChannel().size() > 0) {
-      sum = 0;
-      for (int i = 0; i < edArray.length; i++) {
-        sum += edArray[i].getEventCount();
-      }
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException ex) {
-        Logger.getLogger(Simizer.class.getName()).log(Level.SEVERE, null, ex);
-      }
-    }
-
-    System.out.print("PRINTING TIME: ");
-    for (int i = 1; i < 11; i++) {
-      System.out.print(Simizer.storetime[i] + ";");
-    }
-    System.out.println("");
-
-    System.out.println("Stopping..." + ServerNode.getHits());
-    for (int i = 0; i < edArray.length; i++) {
-      edArray[i].stop();
-    }
-    pol.printAdditionnalStats();
-    es.shutdown();
-  }
+  private static void run(String[] args) {}
 
   private static void generate(String[] args) {
     String lawConfFile = args[0];
@@ -327,109 +179,7 @@ public class Simizer {
     }
   }
 
-  private static void clientSim(String[] args) {
-    String lawConfFile = args[0];
-    String reqDesc = args[1];
-    String resFile = args[2];
-    String nodeFile = args[3];
-    String policyName = args[4];
-    long endSim = Long.parseLong(args[5]);
-    int maxUsers = Integer.parseInt(args[6]);
-    //clientSim wl.conf  ./reqDescription_image_v2.csv ./ressources.json ./10nodes.json simizer.policies.RoundRobin 180000 15
-    runClientSim(lawConfFile, reqDesc, resFile, nodeFile, policyName, endSim, maxUsers);
-  }
-
-  /**
-   * Sets up the clients, ressources and nodes for client based simulation.
-   *
-   * @param lawConfFile
-   * @param rDescFile
-   * @param resFile
-   * @param policy
-   * @param endSim
-   * @param maxUsers
-   */
-  private static void runClientSim(String lawConfFile, String rDescFile, String resFile, String nodeFile, String policyName, long endSim, int maxUsers) {
-
-    Map<String, Law> lawMap = loadLaws(lawConfFile);
-
-    ResourceFactory resfac = SimizerUtils.getRessourceFactory(resFile);
-
-    StorageElement.setFactory(resfac);
-
-    RequestFactory rf = null;
-    try {
-      rf = new RequestFactory(RequestFactory.loadRequests(rDescFile));
-    } catch (IOException ex) {
-      Logger.getLogger(Simizer.class.getName()).log(Level.SEVERE, null, ex);
-      System.exit(0);
-    }
-    ClientNode.configureRequestFactory(rf);
-    ClientNode.configureLaws(
-            lawMap.get("requestLaw"), lawMap.get("thinkTimeLaw"), lawMap.get("durationLaw"));
-    Law internetLatency = new GaussianLaw(20);
-    internetLatency.setParam(10.0);
-
-    Network internet = new Network(internetLatency);
-    //LAN
-    Law lanLatency = new GaussianLaw(10);
-    lanLatency.setParam(5.0);
-    Network lan = new Network(lanLatency);
-    // policy & lb setup :
-    Policy pol = null;
-    try {
-      pol = (Policy) Class.forName(policyName).newInstance();
-      System.out.println(pol.toString());
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      Logger.getLogger(SimizerUtils.class.getName()).log(Level.SEVERE, null, ex);
-      System.exit(0);
-    }
-    Channel c = new Channel();
-
-    LBNode lbn = new LBNode(-1, pol);
-
-    List<ServerNode> nodeList = SimizerUtils.decodeNodes(SimizerUtils.readFile(nodeFile));
-    int nbResPerNode = 1 + (resfac.getResourceNb() / nodeList.size());
-    for (ServerNode n : nodeList) {
-      n.setFrontendNode(lbn);
-//            n.getCapacity();
-      StorageElement se = new StorageElement(
-              ((long) resfac.getMax()) * resfac.getResourceSize(), 4L);
-      se.setPerMBReadDelay(2.0);
-      //se.write(resfac.getStartList(nbResPerNode));
-      se.write(resfac.getStartList());
-      n.setStorage(se);
-
-      n.setNetwork(lan);
-      n.setChannel(c);
-      lan.putNode(n);
-
-    }
-    pol.initialize(nodeList, lbn);
-
-    internet.setChannel(c);
-
-    lan.setChannel(c);
-
-    ClientGenerator cg = new ClientGenerator(internet, lawMap.get("arrivalLaw"), interval, lbn, endSim, maxUsers);
-    lbn.setInsideNetwork(lan);
-    lbn.setOutsideNetwork(internet);
-
-    EventDispatcher ed = new EventDispatcher(c);
-    Thread simThread = new Thread(ed);
-    System.out.println("Starting simulation for " + endSim + " milisecs " + ed.getChannel().size());
-    try {
-
-      simThread.start();
-      simThread.join();
-    } catch (InterruptedException ex) {
-      Logger.getLogger(Simizer.class.getName()).log(Level.SEVERE, null, ex);
-    }
-
-    System.out.println("Stopping..." + ed.getEventCount() + " " + ServerNode.getHits());
-    pol.printAdditionnalStats();
-
-  }
+  private static void clientSim(String[] args) {}
 
   static public Map<String, Law> loadLaws(String lawConfFile) {
     Properties p = new Properties();

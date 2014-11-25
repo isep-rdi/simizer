86080f4d4c0df91431ac3fde62de92ab77d2273a
Max Radermacher <max.radermacher@gmail.com>
2014-11-25 13:30:17 +0100

    Remove ServerNode/LBNode dependencies from Client.
    And also ClientGenerator.

--- src/simizer/ClientNode.java
+++ src/simizer/ClientNode.java
@@ -7,13 +7,15 @@ import simizer.requests.Request;
 import simizer.requests.RequestFactory;
 
 /**
- * This class simulates a web user client machine. It is configured by three
- * laws determining the parameters selection, think time , session duration.
+ * Simulates a client machine.
+ * <p>
+ * All of the instances are configured by three laws determining the resource
+ * selection, "think time" between requests, and the session duration.
+ * <p>
+ * It is also possible to create {@code ClientNode} instances that send a
+ * specified number of requests, rather than sending requests for some duration
+ * of time.
  *
- * Clients will generally send theirs requests to the LBNode corresponding to
- * their attributes DC.
- *
- * @see LBNode
  * @author Sylvain Lefebvre
  */
 public class ClientNode extends Node {
@@ -21,24 +23,24 @@ public class ClientNode extends Node {
   private static Law requestLaw;
   private static Law thinkTimeLaw;
   private static Law durationLaw;
-  private static RequestFactory requestFact;
+  private static RequestFactory requestFactory;
 
   /**
-   * Sets the request factory for the clients
+   * Sets the {@code RequestFactory} used by the clients.
    *
-   * @param rf
+   * @param factory the {@link RequestFactory} for the clients to use
    */
-  public static void configureRequestFactory(final RequestFactory rf) {
-    requestFact = rf;
+  public static void configureRequestFactory(final RequestFactory factory) {
+    ClientNode.requestFactory = factory;
   }
 
   /**
-   * three laws determining: parameters selection, think time , session
-   * duration.
+   * Sets the {@code Law}s determining client behavior.
    *
-   * @param requestLaw
-   * @param thinkTimeLaw
-   * @param durationLaw
+   * @param requestLaw how the client decides which resource to request
+   * @param thinkTimeLaw the amount of time from when the client receives a
+   *            response to when the client sends its next request
+   * @param durationLaw the total amount of time for which the client will exist
    */
   public static void configureLaws(final Law requestLaw, final Law thinkTimeLaw,
         final Law durationLaw) {
@@ -51,7 +53,7 @@ public class ClientNode extends Node {
   private long startTime;
   private long endTime;
   private boolean ended = false;
-  private int requestCount = 0;
+  private int requestCount;
   private final int MAX_REQ = 10;
 
   protected Node serviceAddress;
@@ -65,78 +67,24 @@ public class ClientNode extends Node {
    * @param network
    */
   public ClientNode(int id, Network network, long startTime) {
-    this(id, network, startTime, null);
-  }
-
-  public ClientNode(int id, Network network, long startTime, LBNode lbnode) {
     super(id, network);
     this.startTime = startTime;
-    this.serviceAddress = lbnode;
   }
 
   /**
-   * @deprecated use set service address.
-   * @param lbNode
-   */
-  public void setLBNode(LBNode lbNode) {
-    this.serviceAddress = lbNode;
-  }
-
-  public void setServiceAddress(Node svcNode) {
-    this.serviceAddress = svcNode;
-  }
-
-  /**
-   * Starts the client by scheduling its first request at startTime
-   */
-  @Override
-  public void start() {
-    this.endTime = startTime + durationLaw.nextValue();
-    scheduleNextRequest(this.startTime);
-  }
-
-  /**
-   * reinitialization of the client to be re used by the network.
+   * Reinitialize the client for additional use in the simulation.
+   * <p>
+   * This can avoid needing to instantiate new instances of the class.
    *
-   * @param startTime: long new starting time.
+   * @param startTime the new starting time
    */
   public void reinit(long startTime) {
     this.startTime = startTime;
-    this.endTime = startTime + durationLaw.nextValue();
-    scheduleNextRequest(this.startTime);
+    start();
   }
 
-  /**
-   * On request reception, the node prints out the received request and gets the
-   * next think time, then schedules a request if it does not exceed the
-   * duration law.
-   *
-   * @param timestamp
-   * @param m
-   */
-  @Override
-  public void onMessageReceived(long timestamp, Message m) {
-    Request r = m.getRequest();
-    if (timestamp - r.getArTime() < 0) {
-      System.out.println("PROBLEM");
-    }
-    requestCount++;
-    System.out.println(r.toString()
-        + ";" + (timestamp - r.getArTime())
-        + ";" + getId());
-    //if(timestamp < endTime)
-    if (requestCount < MAX_REQ) {
-      scheduleNextRequest(timestamp);
-    } else {
-      ended = true;
-    }
-  }
-
-  private void scheduleNextRequest(long timestamp) {
-    long nextTime = thinkTimeLaw.nextValue();
-    int tplId = requestLaw.nextValue();
-    Request r = requestFact.getRequest(timestamp + nextTime, tplId);
-    getNetwork().send(this, serviceAddress, r, timestamp + nextTime);
+  public void setServiceAddress(Node serviceNode) {
+    this.serviceAddress = serviceNode;
   }
 
   public long getEndTime() {
@@ -144,7 +92,59 @@ public class ClientNode extends Node {
   }
 
   public boolean getEnded() {
-    return ended;
+    return this.ended;
+  }
+
+  /**
+   * Starts the client by scheduling its first request at startTime.
+   */
+  @Override
+  public void start() {
+    // determine the end time of the client
+    this.endTime = startTime + durationLaw.nextValue();
+    requestCount = 0;
+    scheduleNextRequest(this.startTime);
+  }
+
+  /**
+   * Schedules the next {@code Request}.
+   *
+   * @param timestamp the current timestamp of the simulation
+   */
+  private void scheduleNextRequest(long timestamp) {
+    long nextTime = thinkTimeLaw.nextValue();
+    int templateId = requestLaw.nextValue();
+    Request request = requestFactory.getRequest(timestamp + nextTime, templateId);
+    getNetwork().send(this, serviceAddress, request, timestamp + nextTime);
+  }
+
+  /**
+   * Prints the response, and then schedules the next {@code Request}.
+   * <p>
+   * This method will only schedule the next {@link Request} if it has not yet
+   * reached its completion.
+   *
+   * @param timestamp the current timestamp of the simulation
+   * @param message the message that was received
+   */
+  @Override
+  public void onMessageReceived(long timestamp, Message message) {
+    Request r = message.getRequest();
+    if (timestamp - r.getArTime() < 0) {
+      System.out.println("PROBLEM");
+    }
+
+    System.out.println(r.toString()
+        + ";" + (timestamp - r.getArTime())
+        + ";" + getId());
+    
+    //if(timestamp < endTime)
+    requestCount++;
+    if (requestCount < MAX_REQ) {
+      scheduleNextRequest(timestamp);
+    } else {
+      ended = true;
+    }
   }
 
   @Override
--- src/simizer/network/ClientGenerator.java
+++ src/simizer/network/ClientGenerator.java
@@ -1,13 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizer.network;
 
 import java.util.Iterator;
 import java.util.List;
 import simizer.ClientNode;
-import simizer.LBNode;
 import simizer.Node;
 import simizer.event.ArrivalEvent;
 import simizer.laws.Law;
@@ -18,75 +13,76 @@ import simizer.laws.Law;
  */
 public class ClientGenerator {
 
-  private Law arrivalLaw;
-  protected final LBNode frontend;
+  private final Law arrivalLaw;
+  private final Node frontend;
   private final int interval;
-  private Network nw;
-  private long endSim;
-  private int lastId = 0, maxUsers = 0;
+  private final Network network;
+  private final long endSim;
+  private int lastId = 0;
+  private int maxUsers = 0;
 
-  public ClientGenerator(Network nw, Law arrivalLaw, int interval, LBNode frontend, long endSim, int maxUsers) {
+  public ClientGenerator(Network nw, Law arrivalLaw, int interval, Node frontend, long endSim, int maxUsers) {
     this.arrivalLaw = arrivalLaw;
     this.interval = interval;
     this.frontend = frontend;
-    this.nw = nw;
+    this.network = nw;
     this.maxUsers = maxUsers;
     this.endSim = endSim;
-    nw.registerEvent(new ArrivalEvent(0L, new Long(arrivalLaw.nextValue()), this));
-
+    nw.registerEvent(new ArrivalEvent(0L, (long) arrivalLaw.nextValue(), this));
   }
 
-  private Long reinitNodes(List<Node> nwNodes, Long nb, final long timestamp) {
-    Iterator<Node> itNode = nwNodes.iterator();
-    Node n = itNode.next();
-
-    while (itNode.hasNext() && nb > 0) {
-
-      if (n instanceof ClientNode && ((ClientNode) n).getEnded()) {
-        if (nb > 0) {
-          ((ClientNode) n).reinit(timestamp);
-          nb--;
-        }
+  private Long reinitNodes(List<Node> nodes, long number, final long timestamp) {
+    Iterator<Node> iterator = nodes.iterator();
+    
+    while (iterator.hasNext() && number > 0) {
+      Node node = iterator.next();
+      if (!(node instanceof ClientNode)) {
+        continue;
       }
-      n = itNode.next();
+      ClientNode client = (ClientNode) node;
+
+      if (client.getEnded()) {
+        client.reinit(timestamp);
+        number--;
+      }
     }
-    return nb;
+    
+    return number;
   }
 
   /**
    * This method is called on every interval; It creates new nodes if necessary,
    * and reuses ended nodes to preserve memory.
    *
-   * @param ae : ArrivalEvent
+   * @param event : ArrivalEvent
    */
-  public void onArrivalEvent(long timestamp, ArrivalEvent ae) {
-    List<Node> nodeList = nw.getNodeList();
+  public void onArrivalEvent(long timestamp, ArrivalEvent event) {
+    List<Node> nodeList = network.getNodeList();
     int nbNodes = nodeList.size();
     if (timestamp < endSim) {
-      Long nb = ae.getData();
-      if (nbNodes > 0) {
-        nb = reinitNodes(nodeList, nb, timestamp);
-      }
+      long nodesToCreate = event.getData();
 
-      nb = (nb + nodeList.size()) > maxUsers ? maxUsers - nodeList.size() : nb;
+      // reuse any that are possible
+      nodesToCreate = reinitNodes(nodeList, nodesToCreate, timestamp);
 
-      while (nb > 0) {
+      // make sure that we don't exceed maxUsers
+      nodesToCreate = Math.min(maxUsers - nodeList.size(), nodesToCreate);
+
+      while (nodesToCreate > 0) {
         createNode(lastId++, timestamp);
-        nb--;
+        nodesToCreate--;
       }
-            //System.out.println("Registering next arrival at " + (timestamp+interval));
 
-      nw.registerEvent(
-              new ArrivalEvent(timestamp + interval, (long) arrivalLaw.nextValue(), this));
+      network.registerEvent(
+          new ArrivalEvent(timestamp + interval, (long) arrivalLaw.nextValue(), this));
     }
   }
 
   private void createNode(int id, long timestamp) {
-
-    ClientNode cn = new ClientNode(id, nw, timestamp, frontend);
-    nw.putNode(cn);
-    cn.start();
-
+    ClientNode client = new ClientNode(id, network, timestamp);
+    client.setServiceAddress(frontend);
+    network.putNode(client);
+    client.start();
   }
 
 }

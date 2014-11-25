619ed8a9147bffdaf82305fd3a46c0e34bc0fe29
Max Radermacher <max.radermacher@gmail.com>
2014-11-25 14:06:01 +0100

    Remove utilities for loading ServerNode/Processor.
    These classes are about to disappear, so we should remove the code that
    creates them.

--- src/simizer/utils/SimizerUtils.java
+++ src/simizer/utils/SimizerUtils.java
@@ -6,21 +6,14 @@ package simizer.utils;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.nio.CharBuffer;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import simizer.Node;
-import simizer.processor.Processor;
-import simizer.storage.ResourceFactory;
-import simizer.storage.StorageElement;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import simizer.ServerNode;
+import simizer.storage.ResourceFactory;
 
 /**
  *
@@ -30,69 +23,6 @@ public class SimizerUtils {
     static final JSONParser parser=new JSONParser();
     static  SimizerUtils sim;
     
-    public static List<ServerNode> decodeNodes(String nodesJson) {
-         List<ServerNode> nodes = new ArrayList<>();
-         int id= 0;
-        
-        
-         try {
-            JSONArray array=(JSONArray)parser.parse(nodesJson);
-            
-            for(Object o: array) {
-                JSONObject n = (JSONObject) o;
-                
-                Number nbNode = (Number) n.get("nb");
-                Number cpu = (Number) n.get("cpuSlots");
-                Number mem = (Number) n.get("memorySize");
-                //System.out.println(mem.toString());
-                Number queueSz = (Number) n.get("queueSize");
-                Number cost = (Number) n.get("cost");
-                Number diskSize = (Number) n.get("diskSize");
-                Number nbMips = (Number) n.get("nbMips");
-                String processorNom = (String) n.get("ProcessorName");
-                Processor processor=null;
-                System.out.println(processorNom);
-                
-                for(int i= 0; i< nbNode.intValue();i++) {
-                    StorageElement disk = new StorageElement(diskSize.intValue(),4);
-                    disk.setPerMBReadDelay(2.0);
-                    
-                    processor = createProcessor((int )cpu.longValue(), nbMips.doubleValue(), processorNom);
-                    ServerNode current =new ServerNode(id,
-                        mem.longValue()*1000,
-                        queueSz.intValue(),
-                        cost.doubleValue(), 
-                        disk,
-                        processor);
-                    
-                    processor.setNodeInstance(current);
-                            
-                    nodes.add(current);
-                    id++;
-                }                
-            }
-     
-  
-        } catch (ParseException ex) {
-            Logger.getLogger(SimizerUtils.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return nodes;
-    }
-    public static Processor createProcessor(int core, double nbMips, String pName) {
-        Processor processor = null;
-        try{
-                    
-                Constructor pConst = Class.forName(pName).getConstructor(int.class, double.class);
-                processor =  (Processor) pConst.newInstance(core,nbMips);
-                //System.out.println("Creating Class for :" + pName);
-                }
-                catch(Exception e){
-                    System.out.println("Exception in creating processor instance"
-                            + " at LBSimUtis class :"+e.getMessage());
-                }
-        return processor;
-    }
     public static  String[] readRequestsFile(String fileName) {
         String tmp = readFile(fileName);
         return tmp.split("\\n");
